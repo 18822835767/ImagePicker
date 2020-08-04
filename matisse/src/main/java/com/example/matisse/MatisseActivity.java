e18a4500a9467c31a039a90467e58c20bd01817b
18822835767 <1165407654@qq.com>
2020-08-04 10:16:19 +0800

    各个模块module化，并且添加了依赖

--- matisse/src/main/java/com/example/matisse/MatisseActivity.java
+++ matisse/src/main/java/com/example/matisse/MatisseActivity.java
@@ -1,14 +1,14 @@
-package com.example.imagepicker;
+package com.example.matisse;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MatisseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_matisse);
     }
 }
