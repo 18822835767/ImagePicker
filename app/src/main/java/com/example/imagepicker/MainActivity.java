25949253c2a11bba9f2a2e61016c6863dbc7fa6f
18822835767 <1165407654@qq.com>
2020-08-05 15:13:31 +0800

    写好了一些ui

--- app/src/main/java/com/example/imagepicker/MainActivity.java
+++ app/src/main/java/com/example/imagepicker/MainActivity.java
@@ -8,13 +8,27 @@ import android.app.Service;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.matisse.Matisse;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button mButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.start_activity);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Matisse.from(this).choose().forResult(1);
     }
 }
--- matisse/src/main/java/com/example/matisse/ui/MatisseActivity.java
+++ matisse/src/main/java/com/example/matisse/ui/MatisseActivity.java
@@ -1,8 +1,10 @@
 package com.example.matisse.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.matisse.R;
 
@@ -12,5 +14,20 @@ public class MatisseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matisse);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
