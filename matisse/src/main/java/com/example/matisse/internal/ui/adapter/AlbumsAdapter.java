9df9e64a7151e081c00d917e5d4ed96fdfe30a88
18822835767 <1165407654@qq.com>
2020-08-06 10:12:35 +0800

    写好了下拉列表的适配器

--- matisse/src/main/java/com/example/matisse/internal/ui/adapter/AlbumsAdapter.java
+++ matisse/src/main/java/com/example/matisse/internal/ui/adapter/AlbumsAdapter.java
@@ -7,8 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.matisse.R;
+import com.example.matisse.entity.Album;
+import com.example.matisse.internal.entity.SelectionSpec;
 
 public class AlbumsAdapter extends CursorAdapter {
 
@@ -27,6 +31,13 @@ public class AlbumsAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-
+        Album album = Album.valueOf(cursor);
+        ((TextView)view.findViewById(R.id.album_name)).setText(album.getDisplayName());
+        ((TextView)view.findViewById(R.id.album_media_count)).setText(String.valueOf(album.getCount()));
+        
+        //加载缩略图
+        SelectionSpec.getInstance().imageEngine.loadThumbnail(context,context.getResources().getDimensionPixelSize(R
+                .dimen.media_grid_size),mPlaceholder, 
+                (ImageView) view.findViewById(R.id.album_cover),album.getCoverUri());
     }
 }
