55d00c4208585979749aca2baa1231c3f958bee6
18822835767 <1165407654@qq.com>
2020-08-06 14:49:05 +0800

    加载出下拉列表的缩略图了，好开心

--- picasso/src/main/java/com/example/picasso/ImageUtil/MediaStoreRequestHandler.java
+++ picasso/src/main/java/com/example/picasso/ImageUtil/MediaStoreRequestHandler.java
@@ -1,16 +1,21 @@
 package com.example.picasso.ImageUtil;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.util.Size;
 
 import java.io.IOException;
 
 import static android.content.ContentResolver.SCHEME_CONTENT;
+import static android.provider.MediaStore.Images.Thumbnails.MICRO_KIND;
+import static android.provider.MediaStore.Images.Thumbnails.MINI_KIND;
+import static com.example.picasso.ImageUtil.MediaStoreRequestHandler.PicassoKind.MICRO;
+import static com.example.picasso.ImageUtil.MediaStoreRequestHandler.PicassoKind.MINI;
 
 public class MediaStoreRequestHandler extends RequestHandler {
     private Context context;
@@ -30,8 +35,40 @@ public class MediaStoreRequestHandler extends RequestHandler {
     public Bitmap load(Request request) throws IOException {
         ContentResolver contentResolver = context.getContentResolver();
         
-        //加载图片
-       return contentResolver.loadThumbnail(request.uri,new Size(request.targetWidth,
-                request.targetHeight),null);
+        PicassoKind picassoKind = getPicassoKind(request.targetWidth,request.targetHeight);
+        long id = ContentUris.parseId(request.uri);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        calculateInSampleSize(request.targetWidth,request.targetHeight,picassoKind.width,
+                picassoKind.height,options);
+        
+        return MediaStore.Images.Thumbnails.getThumbnail(contentResolver,id,picassoKind.androidKind,
+                options);
+//        //加载图片
+//       return contentResolver.loadThumbnail(request.uri,new Size(request.targetWidth,
+//                request.targetHeight),null);
+    }
+    
+    static PicassoKind getPicassoKind(int targetWidth,int targetHeight){
+        if(targetWidth <= MICRO.width && targetHeight <= MICRO.height){
+            return MICRO;
+        }else{
+            return MINI;
+        }
+    }
+    
+    enum PicassoKind{
+        MICRO(MICRO_KIND,96,96),
+        MINI(MINI_KIND,512,384);
+        
+        final int androidKind;
+        final int width;
+        final int height;
+
+        PicassoKind(int androidKind, int width, int height) {
+            this.androidKind = androidKind;
+            this.width = width;
+            this.height = height;
+        }
     }
 }
--- picasso/src/main/java/com/example/picasso/ImageUtil/RequestHandler.java
+++ picasso/src/main/java/com/example/picasso/ImageUtil/RequestHandler.java
@@ -1,6 +1,7 @@
 package com.example.picasso.ImageUtil;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import java.io.IOException;
 
@@ -9,5 +10,15 @@ public abstract class RequestHandler {
     
     public abstract Bitmap load(Request request) throws IOException;
     
-    
+    static void calculateInSampleSize(int reqWidth, int reqHeight, int width, int height,
+                                      BitmapFactory.Options options){
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            int heightRatio = Math.round((float) height / (float) reqHeight);
+            int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = Math.min(heightRatio, widthRatio);
+        }
+        options.inSampleSize = inSampleSize;
+        options.inJustDecodeBounds = false;
+    }
 }
